d795e7251ec8020320ae957af6fec7c83ecc542c
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-10-21 14:38:18 +0100

    - Upgraded to use Calendar::Bahai v0.38. - Upgraded to use Calendar::Gregorian v0.09. - Upgraded to use Calendar::Hijri v0.27. - Upgraded to use Calendar::Persian v0.29. - Upgraded to use Calendar::Saka v1.29.

--- lib/Task/Calendar.pm
+++ lib/Task/Calendar.pm
@@ -1,6 +1,6 @@
 package Task::Calendar;
 
-$Task::Calendar::VERSION   = '0.22';
+$Task::Calendar::VERSION   = '0.23';
 $Task::Calendar::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Task::Calendar - Bundles Calendar::* packages.
 
 =head1 VERSION
 
-Version 0.22
+Version 0.23
 
 =head1 DESCRIPTION
 
